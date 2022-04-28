ae691cfb3bd01862e8dfdff8b57e9cdd4ed565ca
Gustavo <gus.inacio@hotmail.com>
2022-04-28 11:12:38 -0300

    feat: Finish the login page structure with data-testids

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import deliveryImage from '../assets/delivery-pic.jpg';
 import deliveryImage2 from '../assets/delivery-image2.jpg';
 
 
-const RootStyle = styled('div')(({ theme }) => ({
+const RootStyle = styled('form')(({ theme }) => ({
   height: '100%',
   display: 'flex',
   justifyContent: 'center',
@@ -18,11 +18,50 @@ export default function Login() {
   return (
     <RootStyle>
       <Box component={'img'} src={deliveryImage2} width={'65%'} height={'100%'} />
-      <Card sx={{ display: 'flex', flex: 1, boxShadow: 4, alignItems: 'center', height: '100%', flexDirection: 'column', justifyContent: 'center' }}>
-        <Typography textAlign={'center'} fontWeight={'bold'} variant="h3" sx={{ mb: 1 }}>Bem Vindo ao App de Delivery</Typography>
-        <TextField sx={{ width: 400, mb: 3 }} />
-        <TextField sx={{ width: 400, mb: 3 }} />
-        <Button variant="contained" sx={{ width: 400 }}>Entrar</Button>
+      <Card sx={{
+        display: 'flex',
+        flex: 1,
+        boxShadow: 4,
+        alignItems: 'center',
+        height: '100%',
+        flexDirection: 'column',
+        justifyContent: 'center'
+      }}
+      >
+        <Typography
+          textAlign={'center'}
+          fontWeight={'bold'}
+          variant="h3"
+          sx={{ mb: 5 }}
+        >
+          Bem Vindo ao App de Delivery
+        </Typography>
+        <TextField
+          data-testid="common_login__input-email"
+          label="Digite o seu E-mail"
+          sx={{ width: 400, mb: 5 }}
+        />
+        <TextField
+          data-testid="common_login__input-password"
+          label="Digite a sua Senha"
+          sx={{ width: 400, mb: 5 }}
+        />
+        <Button
+          data-testid="common_login__button-login"
+          variant="contained"
+          sx={{ width: 400, mb: 3 }}
+        >
+          Entrar
+        </Button>
+        <Box display={'flex'} alignItems={'center'}>
+          <Typography>Ainda não possui uma conta?</Typography>
+          <Button 
+          sx={{ fontWeight: 'bold' }}
+          data-testid="common_login__button-register"
+          >
+            Cadastrar
+          </Button>
+        </Box>
       </Card>
     </RootStyle>
   )
