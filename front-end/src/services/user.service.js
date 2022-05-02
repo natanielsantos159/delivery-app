6bedaf72e25dde2499929a5e549a63759cd96ede
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-05-02 20:20:29 -0300

    feat: Implements post /login request to the api

--- front-end/src/services/user.service.js
+++ front-end/src/services/user.service.js
@@ -1,3 +1,7 @@
-/* import api from './api'; */
+import api from './api';
 
 /* const GET_ALL_PRODUCTS = () => api.get('/products'); */
+
+const LOGIN = (body) => api.post('/login', body);
+
+export default LOGIN;
