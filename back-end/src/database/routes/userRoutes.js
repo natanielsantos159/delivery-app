52e87893eda0e567c021e96805f49a1d15d55d6e
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-05-02 20:12:22 -0300

    feat(routes): Implements endpoint POST /login

--- back-end/src/database/routes/userRoutes.js
+++ back-end/src/database/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userValidation = require('../middlewares/userValidation');
 
 const user = Router();
 
+user.post('/login', userController.login);
 user.post('/register', userValidation.register, userController.register);
 
 module.exports = user;
