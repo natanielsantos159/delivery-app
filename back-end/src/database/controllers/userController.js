b41090c78fa31731daf1906eaad822f84e501c9a
Gustavo <gus.inacio@hotmail.com>
2022-05-02 10:06:04 -0300

    feat: Returns a token on user registration.

--- back-end/src/database/controllers/userController.js
+++ back-end/src/database/controllers/userController.js
@@ -1,10 +1,22 @@
 const user = require('../services/userService')
+const { readFileSync } = require('fs');
+const jwt = require('jsonwebtoken');
+
+const secret = readFileSync('jwt.evaluation.key', 'utf-8');
 
 const register = async (req, res) => {
-  const { name, email, password, role } = req.body;
+  try {
+    const { name, email, password, role } = req.body;
 
-  const response = await user.createUser(name, email, password, role);
-  res.status(201).json(response);
+    await user.createUser(name, email, password, role);
+
+    const token = jwt.sign({ data: { email } }, secret);
+
+    return res.status(201).json({ token });
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
 }
 
 module.exports = {
