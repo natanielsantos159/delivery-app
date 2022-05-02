55186b3d05d9328810c7a1e2b3552021cc6c8077
Gustavo <gus.inacio@hotmail.com>
2022-05-02 18:52:37 -0300

    feat: Save token in local storage.

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -25,74 +25,79 @@ export default function Register() {
   const [password, setPassword] = useState('');
 
   const userRegistration = async () => {
-    const response = await REGISTER_USER({ name, email, password });
-    console.log(response);
+    try {
+      const response = await REGISTER_USER({ name, email, password });
+      const { token } = response.data;
+      localStorage.setItem('user', JSON.stringify(token));
+    } catch (error) {
+
+    }
   };
 
   return (
     <RootStyle>
-      <Box component="img" src={ deliveryImage2 } width="60%" height="100%" />
+      <Box component="img" src={deliveryImage2} width="60%" height="100%" />
       <Card
-        sx={ {
+        sx={{
           display: 'flex',
           flex: 1,
           boxShadow: 4,
           alignItems: 'center',
           height: '100%',
           flexDirection: 'column',
-        } }
+        }}
       >
-        <Box component="img" src={ deliveryMan } width="150px" />
+        <Box component="img" src={deliveryMan} width="150px" />
         <Typography
           textAlign="center"
           fontWeight="bold"
           variant="h4"
-          sx={ { mb: 5 } }
+          sx={{ mb: 5 }}
         >
           Cadastro
         </Typography>
         <TextField
           type="text"
-          error={ name.length > 0 && validateName(name).error }
-          helperText={ name.length > 0
-             && validateName(name).error && validateName(name).message }
-          value={ name }
-          onChange={ ({ target }) => setName(target.value) }
-          inputProps={ { 'data-testid': 'common_register__input-name' } }
+          error={name.length > 0 && validateName(name).error}
+          helperText={name.length > 0
+            && validateName(name).error && validateName(name).message}
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+          inputProps={{ 'data-testid': 'common_register__input-name' }}
           label="Digite o seu Nome"
-          sx={ { width: 350, mb: 5 } }
+          sx={{ width: 350, mb: 5 }}
         />
         <TextField
           type="text"
-          error={ email.length > 0 && validateEmail(email).error }
-          helperText={ email.length > 0
-             && validateEmail(email).error && validateEmail(email).message }
-          value={ email }
-          onChange={ ({ target }) => setEmail(target.value) }
-          inputProps={ { 'data-testid': 'common_register__input-email' } }
+          error={email.length > 0 && validateEmail(email).error}
+          helperText={email.length > 0
+            && validateEmail(email).error && validateEmail(email).message}
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          inputProps={{ 'data-testid': 'common_register__input-email' }}
           label="Digite o seu E-mail"
-          sx={ { width: 350, mb: 5 } }
+          sx={{ width: 350, mb: 5 }}
         />
         <TextField
           type="password"
-          error={ password.length > 0 && validatePassword(password).error }
-          helperText={ password.length > 0
-            && validatePassword(password).error && validatePassword(password).message }
-          value={ password }
-          onChange={ ({ target }) => setPassword(target.value) }
-          inputProps={ { 'data-testid': 'common_register__input-password' } }
+          error={password.length > 0 && validatePassword(password).error}
+          helperText={password.length > 0
+            && validatePassword(password).error && validatePassword(password).message}
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          inputProps={{ 'data-testid': 'common_register__input-password' }}
           label="Digite a sua Senha"
-          sx={ { width: 350, mb: 5 } }
+          sx={{ width: 350, mb: 5 }}
         />
         <Button
           type="submit"
-          onClick={ () => userRegistration() }
-          disabled={ validateName(name).error
-          || validateEmail(email).error
-          || validatePassword(password).error }
+          onClick={() => userRegistration()}
+          disabled={validateName(name).error
+            || validateEmail(email).error
+            || validatePassword(password).error}
           data-testid="common_register__button-register"
           variant="contained"
-          sx={ { width: 350, mb: 3 } }
+          sx={{ width: 350, mb: 3 }}
         >
           Cadastrar
         </Button>
