7e8987b89296d778b7369dc19ab5dbf683149bd4
Nataniel Santos <nathan.santos159@hotmail.com>
2022-05-04 20:56:02 -0300

    refac: Adiciona GET_USER_ORDERS no service do front-end

--- front-end/src/services/user.service.js
+++ front-end/src/services/user.service.js
@@ -5,3 +5,5 @@ export const REGISTER_USER = (body) => api.post('/register', body);
 export const LOGIN = (body) => api.post('/login', body);
 
 export const PRODUCTS = () => api.get('/customer/products');
+
+export const GET_USER_ORDERS = (userId) => api.get(`customer/orders/${userId}`);
