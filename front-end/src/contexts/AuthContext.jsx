1f5d7a7860b3e80ff3c28932f85b14cb885a90a5
Gustavo <gus.inacio@hotmail.com>
2022-05-04 11:00:38 -0300

    feat: Sign up function on auth context

--- front-end/src/contexts/AuthContext.jsx
+++ front-end/src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { createContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LOGIN } from '../services/user.service';
+import { LOGIN, REGISTER_USER } from '../services/user.service';
 
 export const AuthContext = createContext({});
 
@@ -23,8 +23,24 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const validUser = getUserFromStorage();
 
+    setUser(validUser);
     setIsAuthenticated(!!validUser);
   }, []);
+
+  const signUp = async ({ name, email, password }) => {
+    const { data } = await REGISTER_USER({
+      name,
+      email,
+      password,
+    });
+    const { user, token } = data;
+
+    localStorage.setItem('user', JSON.stringify({ ...user, token }));
+
+    setUser(user);
+
+    navigate('/customer/products');
+  };
 
   const signIn = async ({ email, password }) => {
     const { data } = await LOGIN({
@@ -41,7 +57,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={ { isAuthenticated, userInfo, signIn } }>
+    <AuthContext.Provider value={ { isAuthenticated, userInfo, signIn, signUp } }>
       {children}
     </AuthContext.Provider>
   );
