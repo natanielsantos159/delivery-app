a8c07c782c7f423fe983e117d2925172784d7966
Gustavo <gus.inacio@hotmail.com>
2022-05-05 09:16:55 -0300

    style: Product card adjustments.

--- front-end/src/components/products/ProductsCard.jsx
+++ front-end/src/components/products/ProductsCard.jsx
@@ -53,25 +53,28 @@ export default function ProductCard({ item }) {
   useEffect(() => {
     const storageCart = JSON.parse(localStorage.getItem('cart'));
     getTotalValue(storageCart);
-  }, [quantity]);
+  }, [getTotalValue, quantity]);
 
   return (
-    <Card sx={ { maxWidth: 230, borderRadius: 0, marginBottom: 5 } }>
+    <Card
+      sx={ { maxWidth: 230,
+        marginBottom: 5,
+        boxShadow: 6,
+        borderRadius: 2 } }
+    >
       <Box>
         <Typography
-          variant="subtitle1"
-          component="text"
           data-testid={ `customer_products__element-card-price-${id}` }
           sx={ {
-            fontSize: '1.2rem',
+            p: 0.5,
             margin: '4px',
             zIndex: 3,
             position: 'absolute',
-            backgroundColor: 'rgba(10, 0, 0, 0.3)',
+            backgroundColor: 'rgba(122, 120, 120, 0.3)',
             borderRadius: '5px',
           } }
         >
-          { price.toString().replace('.', ',') }
+          { `R$ ${price.toString().replace('.', ',')}` }
         </Typography>
         <CardMedia
           component="img"
@@ -90,6 +93,7 @@ export default function ProductCard({ item }) {
       >
         <Typography
           variant="subtitle1"
+          fontWeight="bold"
           color="text.primary"
           textAlign="center"
           data-testid={ `customer_products__element-card-title-${id}` }
@@ -116,6 +120,7 @@ export default function ProductCard({ item }) {
             id="outlined-read-only-input"
             inputProps={ {
               'data-testid': `customer_products__input-card-quantity-${id}`,
+              style: { textAlign: 'center' },
             } }
             value={ quantity }
             size="small"
