2ca39f94adffec2662da57aca2c8e7d1cebbf39c
Gustavo <gus.inacio@hotmail.com>
2022-05-05 10:43:57 -0300

    fix: Change the product fetching function location.

--- front-end/src/pages/CostumerProducts.jsx
+++ front-end/src/pages/CostumerProducts.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 import CustomerCart from '../components/customer/CustomerCartFloatingBtn';
 import NavBar from '../components/customer/CustomerNavBar';
 import ProductCard from '../components/products/ProductsCard';
-import { PRODUCTS } from '../services/user.service';
+import PRODUCTS from '../services/products.service';
 import useToastManager from '../hooks/useToast';
 
 export default function CostumerProducts() {
   const [products, setProducts] = useState([]);
   const { enqueueToast } = useToastManager();
   const navigate = useNavigate();
-
   const { isAuthenticated } = useAuth();
 
   useEffect(() => {
