01b7863185d7c7804b8e77112cc0df0d087a926b
Nataniel Santos <nathan.santos159@hotmail.com>
2022-05-04 21:40:59 -0300

    feat: Cria model SaleProduct

---
+++ back-end/src/database/models/SaleProduct.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const SaleProduct = sequelize.define('SaleProduct', {
+    saleId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: { model: 'users', key: 'id' },
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: { model: 'products', key: 'id' },
+    },
+    quantity: DataTypes.INTEGER,
+  }, {
+    timestamps: false,
+    tableName: 'sales_products',
+    underscored: true,
+  });
+
+  SaleProduct.associate = (models) => {
+    models.Sale.hasMany(models.Product, {
+      as: 'products', through: SaleProduct, foreignKey: 'saleId', otherKey: 'productId',
+    });
+
+    models.Product.belongsToMany(models.Sale, {
+      as: 'sales', through: SaleProduct, foreignKey: 'productId', otherKey: 'saleId',
+    });
+  }
+
+  return SaleProduct;
+};
