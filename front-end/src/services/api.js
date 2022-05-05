a29400bfd1dbeacb0736523030072a4ba8298ccd
Gustavo <gus.inacio@hotmail.com>
2022-05-05 15:22:16 -0300

    feat: Send token inside headers on requests

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+// Configuração pra interceptar todas as requests e enviar um token nos headers
+api.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user) {
+    config.headers.Authorization = user.token;
+  }
+  return config;
+});
+
 export default api;
