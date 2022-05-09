7b187d056eeb29d9b375311356c9eac4f6947cdb
Gustavo <gus.inacio@hotmail.com>
2022-05-09 18:09:23 -0300

    feat: Get sellers endpoint integration

--- front-end/src/components/ConfirmOrderForm.jsx
+++ front-end/src/components/ConfirmOrderForm.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Card, MenuItem, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import { GET_SELLERS } from '../services/user.service';
+import useToastManager from '../hooks/useToast';
 
 export default function ConfirmOrderForm() {
+  const [sellers, setSellers] = useState([]);
+  const [selectedSeller, setSelectedSeller] = useState('');
+  const { enqueueToast } = useToastManager();
+
+  useEffect(() => {
+    const getSellers = async () => {
+      try {
+        const response = await GET_SELLERS();
+        setSellers(response.data);
+      } catch (error) {
+        console.log(error.message);
+        enqueueToast('error', 'Erro ao carregar os vendedores', 'erro');
+      }
+    };
+    getSellers();
+  }, [enqueueToast]);
+
   return (
     <Card
       sx={ {
@@ -24,13 +43,15 @@ export default function ConfirmOrderForm() {
           label="P.Vendedor Responsável"
           select
           sx={ { width: 200 } }
+          value={ selectedSeller }
+          onChange={ ({ target }) => setSelectedSeller(target.value) }
           inputProps={ { 'data-testid': 'customer_checkout__select-seller' } }
           SelectProps={ { MenuProps:
             { disableScrollLock: true, sx: { maxHeight: 300 } } } }
         >
-          <MenuItem>fulana 1</MenuItem>
-          <MenuItem>fulana 2</MenuItem>
-          <MenuItem>fulana 3</MenuItem>
+          {sellers.map((seller) => (
+            <MenuItem value={ seller.id } key={ seller.id }>{seller.name}</MenuItem>
+          ))}
         </TextField>
         <TextField
           inputProps={ { 'data-testid': 'customer_checkout__input-address' } }
--- front-end/src/services/user.service.js
+++ front-end/src/services/user.service.js
@@ -4,6 +4,6 @@ export const REGISTER_USER = (body) => api.post('/register', body);
 
 export const LOGIN = (body) => api.post('/login', body);
 
-export const PRODUCTS = () => api.get('/customer/products');
-
 export const GET_USER_ORDERS = (userId) => api.get(`customer/orders/${userId}`);
+
+export const GET_SELLERS = () => api.get('/sellers');
