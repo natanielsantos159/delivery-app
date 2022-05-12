a6f2f464cd2431ca9f0bbc5394ddb8ea3cfa285d
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-05-11 23:15:07 -0300

    feat: Implement endpoints to update orders status

--- back-end/src/database/routes/sellerRoutes.js
+++ back-end/src/database/routes/sellerRoutes.js
@@ -6,4 +6,8 @@ const seller = Router();
 
 seller.get('/orders', JWTauth, saleController.listSellerOrders);
 
+seller.put('/orders/preparing/:orderId', JWTauth, saleController.setAsPreparing);
+
+seller.put('/orders/intransit/:orderId', JWTauth, saleController.setAsInTransit);
+
 module.exports = seller;
