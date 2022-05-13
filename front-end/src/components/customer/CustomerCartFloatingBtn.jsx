3cd27d9b45b787a8c65093704321ba6edf813242
Nataniel Santos <nathan.santos159@hotmail.com>
2022-05-13 12:08:16 -0300

    feat: add icon and badge to cart floating button

--- front-end/src/components/customer/CustomerCartFloatingBtn.jsx
+++ front-end/src/components/customer/CustomerCartFloatingBtn.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  Fab, Typography,
+  Fab, Typography, Badge,
 } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import useCart from '../../hooks/useCart';
 
 export default function CustomerCart() {
-  const { totalPrice } = useCart();
+  const { totalPrice, cartItems } = useCart();
   const navigate = useNavigate();
+
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  const getCartTotalQuantity = () => {
+    const totalSum = cartItems.reduce((sum, { quantity }) => sum + quantity, 0);
+    return totalSum;
+  };
 
   return (
     <Fab
@@ -19,12 +30,16 @@ export default function CustomerCart() {
         bottom: 16,
         right: 16,
         color: 'white',
+        columnGap: '10px',
       } }
       color="primary"
       data-testid="customer_products__button-cart"
     >
+      <Badge badgeContent={ getCartTotalQuantity() } color="error">
+        <ShoppingCartIcon />
+      </Badge>
       <Typography data-testid="customer_products__checkout-bottom-value">
-        {String(totalPrice).replace('.', ',')}
+        { formatter.format(totalPrice) }
       </Typography>
     </Fab>
   );
